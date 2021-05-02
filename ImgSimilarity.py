25c53890d5475526755c9c8e7a497149377e6dbf
chunhian349 <83117787+chunhian349@users.noreply.github.com>
2021-05-02 23:41:26 +0800

    Update ImgSimilarity.py

--- ImgSimilarity.py
+++ ImgSimilarity.py
@@ -1,13 +1,17 @@
+###############################################################################################
+# The program is implemented using Visual Studio Code
+# Please make sure open the folder containing the images so that user can direct input the name
+# of image(.png/others) without specifiying its location.
+# The program designed for 2 images with same size for cosine similarity calculation.
+###############################################################################################
+
 #Import Python Imaging Library 
 from PIL import Image
 #Import numpy math library
 import numpy as np
 
-
 ###############################################################################################
-# Data stored is type int8 ranging from (0-255) when convert image into matrix
-# Make sure open the folder containing the image 
-# so that user can direct input the name of image(.png/others) without specifiying its location
+# Function that calculate cosine similarity.
 # dotP : dot product
 # mag : magnitude
 ################################################################################################
@@ -17,9 +21,11 @@ def cosineSimilarity(vect1, vect2):
     mag2 = 0.0
 
     for i in range (len(vect1)) : 
-        dotP += int(vect1[i]) * vect2[i]   
-        mag1 += vect1[i] ** 2 
-        mag2 += vect2[i] ** 2
+        # Need convert into normal integer since pixel are stored in int8
+        # If not, overflow may occur and affect the calculation
+        dotP += int(vect1[i]) * int(vect2[i])   
+        mag1 += int(vect1[i]) ** 2 
+        mag2 += int(vect2[i]) ** 2
 
     mag1 = np.sqrt(mag1) 
     mag2 = np.sqrt(mag2) 
@@ -28,16 +34,19 @@ def cosineSimilarity(vect1, vect2):
 
 
 
-###################################################################
-# Using the formula from the article
-###################################################################
+################################################################################################
+# Function that calculate adjusted cosine similarity.
+# avg : Average pixels in the vector
+# sqrt1 : First square root part of denominator in the formula
+# sqrt2 : Second square root part of denominator in the formula
+################################################################################################
 def adjustedCosineSimilarity(vect1, vect2):
     avg1 = 0.0
     avg2 = 0.0
 
     for i in range (len(vect1)):
-        avg1 += vect1[i]
-        avg2 += vect2[i]
+        avg1 += int(vect1[i])
+        avg2 += int(vect2[i])
 
     avg1 = avg1 / len(vect1)
     avg2 = avg2 / len(vect2)
@@ -47,9 +56,10 @@ def adjustedCosineSimilarity(vect1, vect2):
     sqrt2 = 0.0
 
     for i in range (len(vect1)) : 
+        # Purpose of datatype conversion same as cosine similarity
         dotP += ( int(vect1[i]) - avg1 ) * ( int(vect2[i]) - avg2 )
-        sqrt1 += ( (vect1[i] - avg1) ) ** 2 
-        sqrt2 += ( (vect2[i] - avg2) ) ** 2
+        sqrt1 += ( int(vect1[i]) - avg1 ) ** 2 
+        sqrt2 += ( int(vect2[i]) - avg2 ) ** 2
 
     sqrt1 =  np.sqrt(sqrt1) 
     sqrt2 =  np.sqrt(sqrt2) 
@@ -58,25 +68,31 @@ def adjustedCosineSimilarity(vect1, vect2):
 
 
 
-#######################################################################
-# Read image file, convert into a matrix then return as a vector
-#######################################################################
+################################################################################################
+# Read image file, convert into a matrix then return as a vector.
+################################################################################################
 def imgToVect(img_name) :
     # Read image from opened folder
     img = Image.open(img_name)
 
-    #Convert image into matrices
-    pixel_Matrix = np.array(img)
+    #Convert image into a matrix of pixels
+    pixel_Matrices = np.array(img)
 
-    #Convert matrices into vector
-    vect = np.reshape(  pixel_Matrix, ( len(pixel_Matrix) * len(pixel_Matrix[0]) )  )
+    # Convert matrix into a vector (from 2d array to 1d array)
+    # If mode is 'L', pixels are represented in 1 matrix only
+    if (img.mode == 'L') :
+        vect = np.reshape(  pixel_Matrices, ( len(pixel_Matrices) * len(pixel_Matrices[0]) )  )
+    else : 
+        vect = np.reshape(  pixel_Matrices, ( len(pixel_Matrices) * len(pixel_Matrices[0]) * len(pixel_Matrices[0][0]) )  )
 
     return vect
 
 
 
 #########################################################################
-# Calculate CS and aCS of 2 different images
+# Calculate CS and aCS of 2 different images then display it.
+# Ask user to enter names of 2 images to be compared.
+# Error will occur if the image was not found.
 #########################################################################
 
 def calc_2_Images() : 
@@ -93,9 +109,9 @@ def calc_2_Images() :
 
     print("\n                Calculation Result                ")
     print("**************************************************")
-    print("Cosine similarity: %f" %CS)
-    print("Adjusted cosine similarity: %f" %aCS)
-    print("Differences between CS and aCS: %f" %diff)
+    print("Cosine similarity:                        %f" %CS)
+    print("Adjusted cosine similarity:               %f" %aCS)
+    print("Differences between CS and aCS:           %f" %diff)
     print("**************************************************")
 
     input("\nPress any key to continue...")
@@ -103,10 +119,15 @@ def calc_2_Images() :
 
 
 ##########################################################################################
-# Demonstrate effectiveness of aCS
+# Demonstrate effectiveness of aCS.
+# Modify all pixels become range 0 until 155 and store it in a new copy of vector.
+# Another copy of the modified vector will be used to increase brightness then compare the
+# CS and aCS (between modified vector and brighten vector).
+# Brightness increment from 0 to 100 to show aCS is 1 for every brightness increment.
 ##########################################################################################
 
-def aCS_demostration() :
+def aCS_Demostration() :
+    #User enter name of an image, then read the file and get its vector
     img_name = str(input("Enter filename of an image: "))
 
     ori_Vect = imgToVect(img_name)
@@ -114,21 +135,25 @@ def aCS_demostration() :
     modified_Vect = ori_Vect.copy()
 
     # Modified all pixels become range of 0 until 155
-    # Kind of standardize the pixel to prevent overflow when add with brightness
-    # Real focus is on modified_Vect and brighten_Vect
+    # Prevent overflow when add with brightness
     for i in range (len(modified_Vect)) :
         if (modified_Vect[i] >= 155) :
             modified_Vect[i] -= 100
 
+    # The pixel of this vector will increse by brightness array 
+    # then compare back with modified vector 
     brighten_Vect = modified_Vect.copy()
 
+    # Value of pixel to be added on brighten vector
     brightness = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+    # To store the calculation result for each brightness increment
     CS = np.empty(11)
     aCS = np.empty(11)
     diff = np.empty(11)
 
     for i in range ( len(brightness) ) :
-        #Increase brightness of each pixel 
+        #Increase brightness of each pixel based on value of brightness[i]
         for j in range ( len(brighten_Vect) ) :
             brighten_Vect[j] += brightness[i]
 
@@ -137,35 +162,33 @@ def aCS_demostration() :
         aCS[i] = adjustedCosineSimilarity(modified_Vect, brighten_Vect)
         diff[i] = abs(CS[i] - aCS[i])
 
-        # After calculation, reset the vector back the original value
+        # After calculation, restore the vector back the original value
         # to prevent accumulation of value
         # Analogy: x+10 change back to x then only proceed to x+20
-        if (brightness[i] != 100) :
-            for j in range ( len(brighten_Vect) ) :
-                brighten_Vect[j] -= brightness[i]
+        for j in range ( len(brighten_Vect) ) :
+            brighten_Vect[j] -= brightness[i]
 
-    print("\n                               Table of calculation results                               ")
-    print("******************************************************************************************")
+    # Display all the calculation result 
+    print("\n                                    Calculation results                                   ")
+    print("*******************************************************************************************")
     print("Brightness increment\tCosine similarity\tAdjusted cosine similarity\tDifferences")
-    print("******************************************************************************************")
+    print("*******************************************************************************************")
     for i in range(len(CS)) :
         print('{:<20}'.format(brightness[i]), end='\t')
         print( '{:.6f}{:>8}'.format(CS[i], " "), end='\t')
         print( '{:.6f}{:>17}'.format(aCS[i], " "), end='\t')
         print( '{:.6f}{:>2}'.format(diff[i], " ")   )
 
-    print("******************************************************************************************")
+    print("*******************************************************************************************")
 
-    print("Notice that aCS always 1, means both images are same regardless of brightness")
-    input("Press any key to continue...")
-
-
+    input("\nPress any key to continue...")
 
 #################################################################################################################
-#---------------------------------------------------MAIN---------------------------------------------------------
 
-print("This program will compare images using cosine similarity and adjusted cosine similarity...")
 
+
+#----------------------------------------------MAIN PROGRAM------------------------------------------------------
+# Menu loop allow user to compare similarity as many times as desired 
 while(True): 
     print("\n--------------Cosine Similarity(CS) and Adjusted Cosine Similarity(aCS) calculator-----------------")
     print("1: Calculate CS and aCS of 2 different images")
@@ -180,7 +203,7 @@ while(True):
         calc_2_Images()
 
     elif(choice == 2):
-        aCS_demostration()
+        aCS_Demostration()
 
     else:
         break
